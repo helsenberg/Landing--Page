d9adeda10d9a55453dfc81f68f79ba3ce5bbef90
helsenberg <antonovdiman71@gmail.com>
2022-07-28 00:20:52 +0300

    main

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,42 @@
         <img class="content-block__logo" :src="require('./assets/logo-name.svg')">
         <div>Совсем скоро тут появится информация о нашем новом продукте</div>
         <div class="content-block__scnd-line">Вы можете перейти на <a href="https://st-inv.ru/" style="color: #557745">главную страницу</a>, чтобы ознакомиться с остальными продуктами</div>
-        <v-btn color="#A5CD39" class="button">Демо</v-btn>
+        <v-btn @click="formOpening('true')" color="#A5CD39" class="button">Демо</v-btn>
       </div>
+    </div>
+
+    <div>
+      <v-overlay
+        v-if="overlay === 'true'"
+      >
+        <div class="card">
+          <div class="card__title d-flex justify-space-between mb-10">
+            <h1>Заказать демо</h1>
+            <v-icon @click="formOpening('false')" color="#FF2400" class="pointer">mdi-close</v-icon>
+          </div>
+          <div class="card__content">
+              <div class="d-flex align-center mb-3">Компания</div>
+              <input class="mb-3 mr-1" style="padding: 10px 5px 10px 5px;" type="text" id="value" placeholder="Укажите название компании, номер ИНН или ОГРН">
+              
+              <div class="d-flex align-center mb-3">ФИО</div>
+              <input class="mb-3 mr-1" style="padding: 10px 5px 10px 5px;" type="text" id="value" placeholder="Укажите Фамилию Имя Отчество">
+              
+              <div class="d-flex align-center mb-3">Email</div>
+              <input class="mb-3 mr-1" style="padding: 10px 5px 10px 5px;" type="text" id="value" placeholder="Укажите Email">
+            
+              <div class="d-flex align-center mb-3">Телефон</div>
+              <input class="mb-3 mr-1" style="padding: 10px 5px 10px 5px;" type="text" id="value" placeholder="Укажите Ваш номер телефона">
+              
+              <div class="d-flex align-center mb-3">Количество свиноматок</div>
+              <input class="mb-3 mr-1" style="padding: 10px 5px 10px 5px;" type="text" id="value" placeholder="Укажите количество свиноматок">
+          </div>
+          <div class="d-flex align-center mb-5">
+            <input type="checkbox" class="checkbox mr-3"> 
+            <div>Я согласен с условиями обработки персональных данных</div>
+          </div>
+          <v-btn color="#A5CD39">Заказать</v-btn>
+        </div>
+      </v-overlay>
     </div>
   </v-app>
 </template>
@@ -22,10 +56,12 @@ export default {
   },
 
   data: () => ({
-    
+    overlay: 'false'
   }),
   methods: {
-    
+    formOpening(value) {
+      this.overlay = value
+    }
   },
   props: [
     
@@ -76,5 +112,17 @@ a {
   color: #FFFFFF;
   font-size: 20px;
   border: 1px #404040 solid
+}
+.card {
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.25);
+  border-radius: 10px;
+  background-color: #fff;
+  color: #000;
+  padding: 50px;
+}
+.card__content {
+  display: grid;
+  grid-template-columns: 1fr 2.25fr;
+  column-gap: 25px;
 }
 </style>
